7526502c93be52c3002724812bc7bf2271385005
yigit@mobcodes.com <yigit@mobcodes.com>
2019-05-24 21:34:17 +0300

    post modelinde publish date default current time olarak ayarlandı

--- deneme/models.py
+++ deneme/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            default=timezone.now)
 
     def publish(self):
         self.published_date = timezone.now()
